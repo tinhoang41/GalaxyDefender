276676ec77ea858f0d9eff222fc1606f8598122a
tinhoang41 <tinhoang41@gmail.com>
2016-11-26 08:54:27 -0800

    add global controller

---
+++ Assets/GlobalControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData
+{
+    float totalScore;
+}
+public class GlobalControl : MonoBehaviour {
+
+    public static GlobalControl Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
